247ef383573f1eb999c18274b80409d0bc01f01c
Arpan Patel <arpan.p.se@gmail.com>
2024-06-16 20:56:32 +0000

    adds routes users and add_users, user create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  # display users list
   def index
     @users = User.all.order({ :created_at => :desc })
 
@@ -11,5 +12,22 @@ class UsersController < ApplicationController
     @user = User.where({:id => the_id }).at(0)
 
     render({ :template => "users/show" })
+  end
+
+  def create
+
+    @user = User.new
+    @user.username = params.fetch("input_username")
+
+
+    if @user.valid?
+      @user.save
+      redirect_to("/users", { :notice => "Student created successfully." })
+    else
+      redirect_to("/users", { :notice => "Student failed to create successfully." })
+    end
+
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "users", :action => "index"})
+  post("/add_user", {:controller => "users", :action => "create"})
+  get("/users", { :controller => "users", :action => "index"})
 
 
 
