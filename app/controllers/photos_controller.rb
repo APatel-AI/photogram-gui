8d97f81d4e8eebe1d3ddf68ea1820cb9bf55736a
Arpan Patel <arpan.p.se@gmail.com>
2024-06-17 19:40:35 +0000

    added username details section

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -13,14 +13,16 @@ class PhotosController < ApplicationController
   end
 
   def create
+    
     input_image = params.fetch("input_image")
     input_caption = params.fetch("input_caption")
     input_owner_id = params.fetch("input_owner_id")
     @photo = Photo.new
     @photo.image = input_image
     @photo.caption = input_caption
-    @photo.owner_id = input_owner_id
+    # @photo.owner_id = input_owner_id
     @photo.save
+
     redirect_to("/photos")
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index"})
   post("/add_user", {:controller => "users", :action => "create"})
   get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_id", { :controller => "users", :action => "show"})
 
   #photos
 
